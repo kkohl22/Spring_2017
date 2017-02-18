ad6e4187dc7e9925236a561153e91fb6a3cf9dfc
Ken Kohl <kohlken22@gmai.com>
2017-02-18 16:21:35 -0600

    first commit

---
+++ src/Tuple.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Ken Kohl on 2/18/2017.
+ */
+public class Tuple {
+    Tuple(int keyp, float valueP) {
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getKey() {
+        return 0;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getValue() {
+        return 0;
+    }
+
+    /**
+     *
+     * @param t
+     * @return
+     */
+    public boolean equals(Tuple t) {
+
+    }
+}
